445af67ccacf68ba07519a54a9d880b69f071e07
R Wahyu Pramono <rwpramono@gmail.com>
2022-06-19 21:46:33 +0700

    Inital project setup

---
+++ DotaHeroVIP/Application/AppDelegate.swift
@@ -0,0 +1,22 @@
+//
+//  AppDelegate.swift
+//  DotaHeroVIP
+//
+//  Created by Rachmat Wahyu Pramono on 19/06/22.
+//
+
+import UIKit
+
+@main
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    var window : UIWindow?
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        window = UIWindow(frame: UIScreen.main.bounds)
+        let initialViewController = HomeScreenVC()
+        window?.rootViewController = initialViewController
+        window?.makeKeyAndVisible()
+        return true
+    }
+}
---
+++ DotaHeroVIP/Feature/HomeScreen/HomeScreenVC.swift
@@ -0,0 +1,18 @@
+//
+//  HomeScreenVC.swift
+//  DotaHeroVIP
+//
+//  Created by Rachmat Wahyu Pramono on 19/06/22.
+//
+
+import UIKit
+import Foundation
+
+final class HomeScreenVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        view.backgroundColor = .red
+    }
+}
